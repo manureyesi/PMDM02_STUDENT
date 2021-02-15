3e53eab14348c987c6505583bdbfff7e7dabf026
manureyesi <manureyesi@outlook.es>
2021-02-15 21:43:34 +0100

    Primer commit

---
+++ app/src/main/java/net/iessanclemente/pmdm/u3_student_manu/MainActivity.java
@@ -0,0 +1,14 @@
+package net.iessanclemente.pmdm.u3_student_manu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
