56447f65f86d3148ec000f7a08c0130844a803a8
manureyesi <manureyesi@outlook.es>
2021-02-16 23:02:29 +0100

    Añadir logica a boton cronometro

--- app/src/main/java/net/iessanclemente/pmdm/u3_student_manu/MainActivity.java
+++ app/src/main/java/net/iessanclemente/pmdm/u3_student_manu/MainActivity.java
@@ -3,12 +3,35 @@ package net.iessanclemente.pmdm.u3_student_manu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botonCronometro;
+    private Chronometer cronometro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botonCronometro = findViewById(R.id.buttonCronometro);
+        botonCronometro.setOnClickListener(this::onClickCronometro);
+
+        cronometro = findViewById(R.id.cronometro);
+    }
+
+    /**
+     * Evento OnClick para el Cronometro
+     * @param view
+     */
+    private void onClickCronometro(View view) {
+
+        this.cronometro.setBase(SystemClock.elapsedRealtime());
+        this.cronometro.start();
+
     }
 }
