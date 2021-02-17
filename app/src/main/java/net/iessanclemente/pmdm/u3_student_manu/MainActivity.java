90248fb70c929a8c29c746bdeefe64c4a245c4a2
manureyesi <manureyesi@outlook.es>
2021-02-17 22:12:26 +0100

    Creación de método para la ocultación de la im

--- app/src/main/java/net/iessanclemente/pmdm/u3_student_manu/MainActivity.java
+++ app/src/main/java/net/iessanclemente/pmdm/u3_student_manu/MainActivity.java
@@ -2,16 +2,20 @@ package net.iessanclemente.pmdm.u3_student_manu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button botonCronometro;
     private Chronometer cronometro;
+    private ImageView imagenMonumento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,24 @@ public class MainActivity extends AppCompatActivity {
         botonCronometro.setOnClickListener(this::onClickCronometro);
 
         cronometro = findViewById(R.id.cronometro);
+
+        imagenMonumento = findViewById(R.id.imageViewMonumento);
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        final String TAG = "onConfigurationChanged:";
+
+        //check config
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Log.i(TAG, "Ocultar imagen al modificar orientacion");
+            imagenMonumento.setVisibility(View.INVISIBLE);
+        } else {
+            Log.i(TAG, "Mostrar imagen al modificar orientacion");
+            imagenMonumento.setVisibility(View.VISIBLE);
+        }
+
     }
 
     /**
