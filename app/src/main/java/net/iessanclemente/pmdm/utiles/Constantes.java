734c5d6bc854ad3675833edd3704fb6e03054c4f
manureyesi <manureyesi@outlook.es>
2021-02-18 23:40:47 +0100

    Añadir archivo de contantes para realizar los calculos del crono

---
+++ app/src/main/java/net/iessanclemente/pmdm/utiles/Constantes.java
@@ -0,0 +1,9 @@
+package net.iessanclemente.pmdm.utiles;
+
+public class Constantes {
+
+    public final static Integer TIEMPO_AUTODESTRUCCION_APP = 15;
+
+    public final static Integer CALCULO_ML_TO_S = 1000;
+
+}
